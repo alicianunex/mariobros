cabf49727f3f89222f9b7cdd82cd49d7b20d1f4b
alicianunezisaac <alicianunez10492@gmail.com>
2021-07-15 03:16:07 +0200

    gamelogic

---
+++ logic.js
@@ -0,0 +1,12 @@
+kaboom({
+  global: true,
+  fullscreen: true,
+  scale: 2,
+  debug: true,
+  clearColor: [0, 0, 0, 1],
+});
+
+scene('game', ({ level, score }) => {
+  layers(['bg', 'obj', 'ui'], 'obj');
+});
+start('game', { level: 0, score: 0 });
